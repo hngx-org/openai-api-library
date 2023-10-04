5e0877a2b37fe0ea9e89022ead7f8b9a9b0f7f72
daniel-iroka <danieliroka67@gmail.com>
2023-10-04 16:45:56 +0100

    Migrated Library to use Kotlin Ktor instead of Retrofit.

--- ApiLibrary/src/main/java/com/example/apilibrary/wrapperclass/OpenAiCaller.kt
+++ ApiLibrary/src/main/java/com/example/apilibrary/wrapperclass/OpenAiCaller.kt
@@ -1,29 +1,25 @@
 package com.example.apilibrary.wrapperclass
 
-import com.example.apilibrary.library.PromptChat
 import com.example.apilibrary.library.OpenAiClient
-import com.example.apilibrary.library.Prompt
-import com.example.apilibrary.library.PromptChatCompletion
-import com.example.apilibrary.library.PromptCompletion
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import retrofit2.Response
 
 class OpenAiCaller {
     companion object {
-        suspend fun generateUserChatResponse(userInput: String, savedHistory : Array<String>): Response<PromptChatCompletion> {
-            val openAiClient = OpenAiClient.service
+        suspend fun generateUserChatResponse(userPrompt: String, userPromptChat: Array<String>, userId: String) : String {
+            val openAiClient = OpenAiClient()
             return withContext(Dispatchers.IO) {
-                val response = openAiClient.callUserChatResponse(PromptChat(user_input = userInput, history = savedHistory))
-                return@withContext response
+                return@withContext openAiClient.getUserChatResponse(
+                    userPrompt,
+                    userPromptChat,
+                    userId
+                )
             }
         }
-
-        suspend fun generateChatResponse(userInput: String): Response<PromptCompletion> {
-            val openAiClient = OpenAiClient.service
+        suspend fun generateChatResponse(userPrompt: String, userId: String) : String {
+            val openAiClient = OpenAiClient()
             return withContext(Dispatchers.IO) {
-                val response = openAiClient.callChatResponse(Prompt(user_input = userInput))
-                return@withContext response
+                return@withContext openAiClient.getChatResponse(userPrompt, userId)
             }
         }
     }
